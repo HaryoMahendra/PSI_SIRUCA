a874852908ddcbee0b810fd3dc563ba74106d989
Haryo Mahendra jati <haryomahendra41@gmail.com>
2024-04-21 05:37:59 +0700

    Edit dashboard

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -192,8 +192,8 @@
             <a href="index3.html" class="brand-link">
                 <img src="{{ asset('picture/logo2.png') }}" alt="SIRUCA Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">SIRUCA</span>
-            </a>
+                <span class="brand-text font-weight-bold" style="font-family: Arial;">SIRUCA</span>
+            </a>                      
 
             <!-- Sidebar -->
             <div class="sidebar">
@@ -204,7 +204,7 @@
                             alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Ajeng</a>
+                        <a href="#" class="d-block">Alexander Pierce</a>
                     </div>
                 </div>
 
@@ -236,7 +236,7 @@
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('admin.index') }}" class="nav-link">
-                                <i class="nav-icon fas fa-user"></i> <!-- Ganti kelas ikon menjadi fas fa-user -->
+                                <i class="nav-icon fas fa-user"></i> 
                                 <p>
                                     Data Users
                                 </p>
@@ -244,10 +244,37 @@
                         </li>
 
                         <li class="nav-item">
-                            <a href="{{ route('admin.index') }}" class="nav-link">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-user-friends"></i>
+                                <p>
+                                    Data Pengunjung
+                                </p>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
                                 <i class="nav-icon fas fa-book"></i> 
                                 <p>
                                     Data Buku
+                                </p>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-book-reader"></i>
+                                <p>
+                                    Data Peminjaman
+                                </p>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-undo"></i> 
+                                <p>
+                                    Data Pengembalian
                                 </p>
                             </a>
                         </li>
@@ -271,13 +298,10 @@
         <!-- Content Wrapper. Contains page content -->
         @yield('content')
         <!-- /.content-wrapper -->
-        <footer class="main-footer">
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+        <footer class="main-footer" style="text-align: center;">
+            <strong>Copyright &copy; 2024 <a href="https://siruca.co.id">SIRUCA</a>.</strong>
             All rights reserved.
-            <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.2.0
-            </div>
-        </footer>
+        </footer>        
 
         <!-- Control Sidebar -->
         <aside class="control-sidebar control-sidebar-dark">
